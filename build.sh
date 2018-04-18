a6e8926f2d2664d84d2a9e84f19f193516c784ca
d.astafyev <bnhf007>
2018-04-18 20:11:11 +0300

    add .env for fast deploy

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ rm -rf app/var/cache
 git clone https://github.com/SkvokeN/BillingTest.git app
 docker-compose up -d
 sh run.sh php-cli composer install --no-interaction
+cat certificates/.env > app/.env
+
+
 sh run.sh php-cli bin/console doctrine:migrations:migrate
 sh run.sh php-cli bin/console doctrine:fixtures:load
